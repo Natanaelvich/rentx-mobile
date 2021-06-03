267acfcd8ea23653cb6d5a3487a5e6ac01615d47
Natanael Lima <taelima1997@gmail.com>
2021-06-03 08:38:42 -0300

    add SchedulingDatails screen

---
+++ src/screens/SchedulingDatails/index.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
+
+import { useTheme } from 'styled-components';
+
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Accessories,
+  Footer,
+  RentalPeriod,
+  CalendarIcon,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  RentalPrice,
+  RentalPriceLabel,
+  RentalPriceDetails,
+  RentalPriceQuota,
+  RentalPriceTotal,
+} from './styles';
+
+export function SchedulingDatails() {
+  const theme = useTheme();
+  const nativation = useNavigation();
+
+  function handleRentalComplete() {
+    nativation.navigate('SchedulingComplete');
+  }
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png',
+          ]}
+        />
+      </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao Dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+        <Accessories>
+          <Accessory name="380Km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 HP" icon={forceSvg} />
+          <Accessory name="Gaoslina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 pessoas" icon={peopleSvg} />
+          <RentalPeriod>
+            <CalendarIcon>
+              <Feather
+                name="calendar"
+                size={RFValue(24)}
+                color={theme.colors.shape}
+              />
+            </CalendarIcon>
+            <DateInfo>
+              <DateTitle>DE</DateTitle>
+              <DateValue>01/07/2021</DateValue>
+            </DateInfo>
+            <Feather
+              name="chevron-right"
+              size={RFValue(10)}
+              color={theme.colors.text}
+            />
+            <DateInfo>
+              <DateTitle>ATÉ</DateTitle>
+              <DateValue>01/07/2021</DateValue>
+            </DateInfo>
+          </RentalPeriod>
+          <RentalPrice>
+            <RentalPriceLabel>TOTAL</RentalPriceLabel>
+            <RentalPriceDetails>
+              <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+              <RentalPriceTotal>R$ 2.900</RentalPriceTotal>
+            </RentalPriceDetails>
+          </RentalPrice>
+        </Accessories>
+      </Content>
+      <Footer>
+        <Button
+          title="Alugar Agora"
+          onPress={handleRentalComplete}
+          color={theme.colors.success}
+        />
+      </Footer>
+    </Container>
+  );
+}
