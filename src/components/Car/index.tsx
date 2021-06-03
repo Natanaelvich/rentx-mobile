0e485f7cdd4d1fa2cb8f79242ba50efee6be94d0
Natanael Lima <taelima1997@gmail.com>
2021-06-03 08:40:25 -0300

    update car with rect button

--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
 import GaoslineSvg from '../../assets/gasoline.svg';
 import {
@@ -23,12 +24,12 @@ interface CarData {
   };
   thumbnail: string;
 }
-interface Props {
+interface Props extends RectButtonProps {
   data: CarData;
 }
-export function Car({ data }: Props) {
+export function Car({ data, ...rest }: Props) {
   return (
-    <Container>
+    <Container {...rest}>
       <Details>
         <Brand>{data.brand}</Brand>
         <Name>{data.name}</Name>
