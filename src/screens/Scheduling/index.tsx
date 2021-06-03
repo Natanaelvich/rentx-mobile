d85fd84312408a0b37eb9ff5e32c0ba3cc3bf5c1
Natanael Lima <taelima1997@gmail.com>
2021-06-02 23:59:09 -0300

    add Scheduling screen

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useTheme } from 'styled-components';
+
+import { StatusBar } from 'react-native';
+import { BackButton } from '../../components/BackButton';
+import { Calendar } from '../../components/Calendar';
+
+import ArrowSvg from '../../assets/arrow.svg';
+import {
+	Container,
+	Header,
+	Title,
+	RentalPeriod,
+	DateInfo,
+	DateTitle,
+	DateValue,
+	Content,
+	Footer,
+} from './styles';
+import { Button } from '../../components/Button';
+
+export function Scheduling(){
+	const theme = useTheme()
+	return (
+		<Container>
+			<StatusBar
+				barStyle="light-content"
+				translucent
+				backgroundColor="transparent"
+			/>
+			<Header>
+				<BackButton
+					onPress={() => {}}
+					color={theme.colors.shape}
+				/>
+				<Title>
+					Escolha uma {`\n`}
+					data de inicio e {`\n`}
+					fim do aluguel
+				</Title>
+				<RentalPeriod>
+					<DateInfo>
+						<DateTitle>DE</DateTitle>
+						<DateValue selected={false}>
+							01/06/2021
+						</DateValue>
+					</DateInfo>
+
+					<ArrowSvg />
+
+					<DateInfo>
+						<DateTitle>Até</DateTitle>
+						<DateValue selected={false}>
+							01/06/2021
+						</DateValue>
+					</DateInfo>
+				</RentalPeriod>
+			</Header>
+			<Content>
+				<Calendar />
+			</Content>
+			<Footer>
+				<Button
+					title="Confirmar"
+				/>
+			</Footer>
+		</Container>
+
+	)
+
+}
---
+++ src/screens/Scheduling/styles.ts
@@ -0,0 +1,73 @@
+import styled, { css } from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+interface DateValueProps {
+	selected : boolean;
+}
+
+export const Container = styled.View`
+	flex: 1;
+	background-color: ${({ theme }) => theme.colors.background_secondary};
+`;
+export const Header = styled.View`
+	width: 100%;
+	height: ${RFValue(325)}px;
+
+	background-color: ${({ theme }) => theme.colors.header};
+	justify-content: center;
+	padding: 25px;
+	padding-top: ${getStatusBarHeight() + 30}px;
+
+
+`;
+
+export const Title = styled.Text`
+	color: ${({ theme }) => theme.colors.shape};
+	font-family: ${({ theme }) => theme.fonts.secondary_600};
+	font-size: ${RFValue(34)}px;
+
+	padding-top: 24px;
+`;
+
+
+export const RentalPeriod = styled.View`
+	width: 100%;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	padding: 24px 0;
+`;
+
+export const DateInfo = styled.View`
+	width: 30%;
+`;
+
+export const DateTitle = styled.Text`
+	color: ${({ theme }) => theme.colors.text};
+	font-family: ${({ theme }) => theme.fonts.secondary_500};
+	font-size: ${RFValue(10)}px;
+`;
+
+export const DateValue = styled.Text<DateValueProps>`
+	color: ${({ theme }) => theme.colors.shape};
+	font-family: ${({ theme }) => theme.fonts.primary_500};
+	font-size: ${RFValue(15)}px;
+
+	${({ selected, theme }) => !selected && css`
+		border-bottom-width: 1px;
+		border-bottom-color: ${theme.colors.text};
+		padding-bottom: 5px;
+	`}
+`;
+
+export const Content = styled.ScrollView.attrs({
+	contentContainterStyle: {
+		paddingBottom: 24
+	},
+	showsVerticalScrollIndicator: false
+})``;
+
+export const Footer = styled.View`
+	padding: 24px;
+`;
