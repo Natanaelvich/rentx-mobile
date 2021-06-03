97ae72f9cc8ecb982a1ec284442a3720fe6a62d9
Natanael Lima <taelima1997@gmail.com>
2021-06-03 08:40:06 -0300

    update button with theme

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useTheme } from 'styled-components';
 
 import { Container, Title } from './styles';
 
 interface Props {
   title: string;
   color?: string;
+  onPress: () => void;
 }
 
-export function Button({ title, color, ...rest }: Props) {
+export function Button({ title, color, onPress, ...rest }: Props) {
+  const theme = useTheme();
   return (
-    <Container {...rest} color={color}>
+    <Container {...rest} color={color || theme.colors.main} onPress={onPress}>
       <Title> {title} </Title>
     </Container>
   );
