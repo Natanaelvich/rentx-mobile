5cf0d39053d2083d32355c5ae2af002e283b119e
Natanael Lima <taelima1997@gmail.com>
2021-06-04 08:25:27 -0300

    add generateInterval calendar

---
+++ src/components/Calendar/generateInterval.ts
@@ -0,0 +1,33 @@
+import { eachDayOfInterval, format } from 'date-fns';
+
+import { MarketDatesProps, DayProps } from '.';
+
+import theme from '../../styles/theme';
+import { getPlatformDate } from '../../utils/getPlatformDate';
+
+export function generateInterval(start: DayProps, end: DayProps) {
+  let interval: MarketDatesProps = {};
+
+  eachDayOfInterval({
+    start: new Date(start.timestamp),
+    end: new Date(end.timestamp),
+  }).forEach(item => {
+    const date = format(getPlatformDate(item), 'yyyy-MM-dd');
+    interval = {
+      ...interval,
+      [date]: {
+        color:
+          start.dateString === date || end.dateString === date
+            ? theme.colors.main
+            : theme.colors.main_light,
+
+        textColor:
+          start.dateString === date || end.dateString === date
+            ? theme.colors.main_light
+            : theme.colors.main,
+      },
+    };
+  });
+
+  return interval;
+}
