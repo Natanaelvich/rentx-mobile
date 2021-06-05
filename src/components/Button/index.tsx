ba97bf0b0b7c9a805d6196653322ecdb905b5bb0
Natanael Lima <taelima1997@gmail.com>
2021-06-05 11:38:30 -0300

    add new props to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
 import { useTheme } from 'styled-components';
-
 import { Container, Title } from './styles';
 
 interface Props {
   title: string;
   color?: string;
   onPress: () => void;
+  enabled?: boolean;
+  loading?: boolean;
 }
 
-export function Button({ title, color, onPress, ...rest }: Props) {
+export function Button({
+  title,
+  color,
+  onPress,
+  enabled = true,
+  loading = false,
+  ...rest
+}: Props) {
   const theme = useTheme();
   return (
-    <Container {...rest} color={color || theme.colors.main} onPress={onPress}>
-      <Title> {title} </Title>
+    <Container
+      {...rest}
+      color={color || theme.colors.main}
+      onPress={onPress}
+      enabled={enabled}
+      style={{ opacity: enabled === false || loading === true ? 0.5 : 1 }}
+    >
+      {loading ? (
+        <ActivityIndicator color={theme.colors.shape} size="small" />
+      ) : (
+        <Title> {title} </Title>
+      )}
     </Container>
   );
 }
