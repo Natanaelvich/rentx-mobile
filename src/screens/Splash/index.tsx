95b8c186a13e49996315c1c4096bc29a868a1a76
Natanael Lima <taelima1997@gmail.com>
2021-06-05 11:34:38 -0300

    add Splash screen

---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  interpolate,
+  Extrapolate,
+  runOnJS,
+} from 'react-native-reanimated';
+import { useNavigation } from '@react-navigation/core';
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
+
+import { Container } from './styles';
+
+export function Splash() {
+  const splashAnimation = useSharedValue(0);
+  const navigation = useNavigation();
+
+  const brandStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(splashAnimation.value, [0, 50], [1, 0]),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [0, -50],
+            Extrapolate.CLAMP,
+          ),
+        },
+      ],
+    };
+  });
+
+  const logoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, 0.3, 1]),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [-50, 0],
+            Extrapolate.CLAMP,
+          ),
+        },
+      ],
+    };
+  });
+
+  function startApp() {
+    navigation.navigate('Home');
+  }
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(50, { duration: 1000 }, () => {
+      'worklet';
+
+      runOnJS(startApp)();
+    });
+  }, []);
+  return (
+    <Container>
+      <Animated.View style={[brandStyle, { position: 'absolute' }]}>
+        <BrandSvg width={80} height={50} />
+      </Animated.View>
+      <Animated.View style={[logoStyle, { position: 'absolute' }]}>
+        <LogoSvg width={180} height={20} />
+      </Animated.View>
+    </Container>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+});
