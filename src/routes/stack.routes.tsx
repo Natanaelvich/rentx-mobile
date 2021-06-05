3a6c06726db923ae21b4a2471fa61b2c2ae0f748
Natanael Lima <taelima1997@gmail.com>
2021-06-05 11:35:03 -0300

    add MyCars Splash to routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,17 +7,27 @@ import { CarDatails } from '../screens/CarDatails';
 import { Scheduling } from '../screens/Scheduling';
 import { SchedulingComplete } from '../screens/SchedulingComplete';
 import { SchedulingDatails } from '../screens/SchedulingDatails';
+import { MyCars } from '../screens/MyCars';
+import { Splash } from '../screens/Splash';
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function StackRoutes() {
   return (
-    <Navigator headerMode="none">
-      <Screen name="Home" component={Home} />
+    <Navigator headerMode="none" initialRouteName="Splash">
+      <Screen name="Splash" component={Splash} />
+      <Screen
+        name="Home"
+        component={Home}
+        options={{
+          gestureEnabled: false,
+        }}
+      />
       <Screen name="CarDatails" component={CarDatails} />
       <Screen name="Scheduling" component={Scheduling} />
       <Screen name="SchedulingComplete" component={SchedulingComplete} />
       <Screen name="SchedulingDatails" component={SchedulingDatails} />
+      <Screen name="MyCars" component={MyCars} />
     </Navigator>
   );
 }
