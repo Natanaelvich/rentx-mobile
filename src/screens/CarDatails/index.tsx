c1be267d2706f72a49b45da4d98a802807ed69a9
Natanael Lima <taelima1997@gmail.com>
2021-06-05 11:36:02 -0300

    add aniamtions CarDatails

--- src/screens/CarDatails/index.tsx
+++ src/screens/CarDatails/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { StatusBar, StyleSheet } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Animated, {
+  useSharedValue,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  interpolate,
+  Extrapolate,
+} from 'react-native-reanimated';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
+import { useTheme } from 'styled-components';
 import { useNavigation, useRoute } from '@react-navigation/native';
+
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 import { Accessory } from '../../components/Accessory';
 import { Button } from '../../components/Button';
 
+import { getAcessoryIcons } from '../../utils/getAcessoryIcons';
+import { CarDTO } from '../../dtos/CarDTO';
+
 import {
   Container,
   Header,
   CarImages,
-  Content,
   Details,
   Description,
   Brand,
@@ -22,8 +36,6 @@ import {
   Accessories,
   Footer,
 } from './styles';
-import { CarDTO } from '../../dtos/CarDTO';
-import { getAcessoryIcons } from '../../utils/getAcessoryIcons';
 
 interface Params {
   car: CarDTO;
@@ -32,7 +44,30 @@ export function CarDatails() {
   const navigation = useNavigation();
   const route = useRoute();
   const { car } = route.params as Params;
-
+  const scrollY = useSharedValue(0);
+  const sizes = {
+    initial: RFValue(70),
+    finally: RFValue(200),
+  };
+  const theme = useTheme();
+  const scrollHandle = useAnimatedScrollHandler(event => {
+    scrollY.value = event.contentOffset.y;
+  });
+  const HeaderStyleAnimation = useAnimatedStyle(() => {
+    return {
+      height: interpolate(
+        scrollY.value,
+        [0, sizes.finally],
+        [sizes.finally, sizes.initial],
+        Extrapolate.CLAMP,
+      ),
+    };
+  });
+  const slidercarStyleAnimation = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(scrollY.value, [0, 150], [1, 0], Extrapolate.CLAMP),
+    };
+  });
   function handleSelectPeriod() {
     navigation.navigate('Scheduling', { car });
   }
@@ -41,13 +76,36 @@ export function CarDatails() {
   }
   return (
     <Container>
-      <Header>
-        <BackButton onPress={handleBack} />
-      </Header>
-      <CarImages>
-        <ImageSlider imagesUrl={car.photos} />
-      </CarImages>
-      <Content>
+      <StatusBar
+        barStyle="dark-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Animated.View
+        style={[
+          HeaderStyleAnimation,
+          styles.header,
+          { backgroundColor: theme.colors.background_secondary },
+        ]}
+      >
+        <Header>
+          <BackButton onPress={handleBack} />
+        </Header>
+        <Animated.View style={[slidercarStyleAnimation]}>
+          <CarImages>
+            <ImageSlider imagesUrl={car.photos} />
+          </CarImages>
+        </Animated.View>
+      </Animated.View>
+      <Animated.ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          paddingTop: getStatusBarHeight() + RFValue(160),
+        }}
+        showsVerticalScrollIndicator={false}
+        onScroll={scrollHandle}
+        scrollEventThrottle={16}
+      >
         <Details>
           <Description>
             <Brand>{car.brand}</Brand>
@@ -67,8 +125,17 @@ export function CarDatails() {
             />
           ))}
         </Accessories>
-        <About>{car.about}</About>
-      </Content>
+        <About>
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+          {car.about}
+        </About>
+      </Animated.ScrollView>
       <Footer>
         <Button
           title="Escolher período do alugel"
@@ -78,3 +145,11 @@ export function CarDatails() {
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    position: 'absolute',
+    overflow: 'hidden',
+    zIndex: 1,
+  },
+});
