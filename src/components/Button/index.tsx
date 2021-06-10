892cb673b3cd9e8144465a1033f59bdb0f19cd35
Natanael Lima <taelima1997@gmail.com>
2021-06-09 22:56:30 -0300

    udpate Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ActivityIndicator } from 'react-native';
 import { useTheme } from 'styled-components';
+
 import { Container, Title } from './styles';
 
 interface Props {
@@ -9,6 +10,7 @@ interface Props {
   onPress: () => void;
   enabled?: boolean;
   loading?: boolean;
+  light?: boolean;
 }
 
 export function Button({
@@ -17,21 +19,21 @@ export function Button({
   onPress,
   enabled = true,
   loading = false,
-  ...rest
+  light = false,
 }: Props) {
   const theme = useTheme();
+
   return (
     <Container
-      {...rest}
-      color={color || theme.colors.main}
       onPress={onPress}
+      color={color || theme.colors.main}
       enabled={enabled}
       style={{ opacity: enabled === false || loading === true ? 0.5 : 1 }}
     >
       {loading ? (
-        <ActivityIndicator color={theme.colors.shape} size="small" />
+        <ActivityIndicator color={theme.colors.shape} />
       ) : (
-        <Title> {title} </Title>
+        <Title light={light}>{title}</Title>
       )}
     </Container>
   );
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,21 +1,29 @@
-import styled from 'styled-components/native';
-import { RFValue } from 'react-native-responsive-fontsize';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
 
 interface ButtonProps extends RectButtonProps {
   color?: string;
 }
 
+interface ButtonTextProps {
+  light?: boolean;
+}
+
 export const Container = styled(RectButton)<ButtonProps>`
   width: 100%;
+
   padding: 19px;
   align-items: center;
   justify-content: center;
-  background-color: ${({ color, theme }) => color || theme.colors.main};
+
+  background-color: ${({ color }) => color};
+  margin-bottom: 8px;
 `;
 
-export const Title = styled.Text`
+export const Title = styled.Text<ButtonTextProps>`
   font-family: ${({ theme }) => theme.fonts.primary_500};
   font-size: ${RFValue(15)}px;
-  color: ${({ theme }) => theme.colors.shape};
+  color: ${({ theme, light }) =>
+    light ? theme.colors.header : theme.colors.shape};
 `;
