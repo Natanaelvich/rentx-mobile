0b6fb4d78a47f00e6793a3f8b7dd5a1813a0a019
Natanael Lima <taelima1997@gmail.com>
2021-06-11 15:05:06 -0300

    add Bullet component

---
+++ src/components/Bullet/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './styles';
+
+interface BulletProps {
+  active?: boolean;
+}
+
+export function Bullet({ active = false }: BulletProps) {
+  return <Container active={active} />;
+}
