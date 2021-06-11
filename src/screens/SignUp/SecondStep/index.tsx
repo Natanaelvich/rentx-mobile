072b2e70b4925afa9e3792fb597de66dcba30077
Natanael Lima <taelima1997@gmail.com>
2021-06-11 15:45:09 -0300

    update SecondStep

--- src/screens/SignUp/SecondStep/index.tsx
+++ src/screens/SignUp/SecondStep/index.tsx
@@ -1,28 +1,57 @@
 import React, { useState } from 'react';
 import * as Yup from 'yup';
 import { Alert } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
 import { BackButton } from '../../../components/BackButton';
 import { Button } from '../../../components/Button';
 import { PasswordInput } from '../../../components/PasswordInput';
-import { Container, Footer, Form, Header, Title } from './styles';
+import {
+  Container,
+  Footer,
+  Form,
+  Header,
+  Step,
+  Steps,
+  Subtitle,
+  Title,
+} from './styles';
+import { Bullet } from '../../../components/Bullet';
+
+interface Params {
+  user: {
+    name: string;
+    email: string;
+    cnh: string;
+  };
+}
 
 export function SecondStep() {
   const [password, setPassword] = useState('');
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const route = useRoute();
+  const { user } = route.params as Params;
   const navigation = useNavigation();
+  const theme = useTheme();
+
   async function handleSignUp() {
     try {
       const schema = Yup.object().shape({
-        password: Yup.string()
-          .required('Insira a senha')
-          .min(6, 'Insira uma senha forte com no mínimo 6 caracteres'),
         passwordConfirmation: Yup.string()
           .required('Insira a confirmação da senha')
           .min(6, 'Insira uma senha forte com no mínimo 6 caracteres')
           .oneOf([Yup.ref('password'), null], 'As senhas devem ser coincidir'),
+        password: Yup.string()
+          .required('Insira a senha')
+          .min(6, 'Insira uma senha forte com no mínimo 6 caracteres'),
       });
-      await schema.validate({ password, passwordConfirmation });
+      const result = await schema.validate({ password, passwordConfirmation });
+      const pageData = {
+        title: 'Conta criada',
+        message: 'Agora é só realizar o login',
+        navigateTo: 'SignIn',
+      };
+      navigation.navigate('Success', { data: pageData });
     } catch (error) {
       if (error instanceof Yup.ValidationError) {
         Alert.alert('Opa', error.message);
@@ -38,9 +67,15 @@ export function SecondStep() {
     <Container>
       <Header>
         <BackButton onPress={handleGoBack} />
+        <Steps>
+          <Bullet />
+          <Bullet active />
+        </Steps>
       </Header>
+      <Title>Crie sua {'\n'}conta</Title>
+      <Subtitle>Faça seu cadastro de{'\n'}forma rápida e fácil</Subtitle>
       <Form>
-        <Title>2.Senha</Title>
+        <Step>2. Senha</Step>
         <PasswordInput
           iconName="lock"
           placeholder="Senha"
@@ -51,7 +86,7 @@ export function SecondStep() {
         />
         <PasswordInput
           iconName="lock"
-          placeholder="Confirmar a senha"
+          placeholder="Repetir senha"
           autoCorrect={false}
           onChangeText={setPasswordConfirmation}
           autoCapitalize="none"
@@ -59,7 +94,11 @@ export function SecondStep() {
         />
 
         <Footer>
-          <Button title="Próximo" onPress={handleSignUp} />
+          <Button
+            title="Confirmar"
+            onPress={handleSignUp}
+            color={theme.colors.success}
+          />
         </Footer>
       </Form>
     </Container>
--- src/screens/SignUp/SecondStep/styles.ts
+++ src/screens/SignUp/SecondStep/styles.ts
@@ -3,16 +3,21 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  width: 100%;
+  padding: 0 24px;
 `;
 export const Header = styled.View`
   margin-top: ${getStatusBarHeight() + 32}px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
-export const Form = styled.View`
-  padding: 0 24px;
+export const Form = styled.View``;
+export const Steps = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 16px;
 `;
-
-export const Title = styled.Text`
+export const Step = styled.Text`
   margin-top: 48px;
   margin-bottom: 24px;
   font-size: ${RFValue(20)}px;
@@ -20,4 +25,17 @@ export const Title = styled.Text`
 `;
 export const Footer = styled.View`
   margin-top: 16px;
+`;
+export const Title = styled.Text`
+  font-size: ${RFValue(40)}px;
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  color: ${({ theme }) => theme.colors.title};
+  margin-top: 32px;
+`;
+export const Subtitle = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
+  margin-top: 16px;
+  line-height: ${RFValue(25)}px;
 `;
