c4197519a777c94e5961b2288ab2f16443897074
Natanael Lima <taelima1997@gmail.com>
2021-06-13 10:22:22 -0300

    add iconButton  component

---
+++ src/components/IconButton/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Feather } from '@expo/vector-icons'
+import { useTheme } from 'styled-components'
+import { useNavigation } from '@react-navigation/native'
+
+import * as S from './styles'
+import { IconButtonProps } from './types'
+
+export const IconButton = (props: IconButtonProps) => {
+  const { name = 'chevron-left', color, onPress, ...attrs } = props
+  const { colors } = useTheme()
+  const { goBack } = useNavigation()
+
+  return (
+    <S.Container onPress={onPress || goBack} {...attrs}>
+      <Feather name={name} color={color || colors.text} size={28} />
+    </S.Container>
+  )
---
+++ src/components/IconButton/types.ts
@@ -0,0 +1,8 @@
+import { ComponentProps } from 'react'
+
+import { Feather } from '@expo/vector-icons'
+import { BorderlessButtonProps } from 'react-native-gesture-handler'
+
+export type IconButtonProps = BorderlessButtonProps & {
+  name?: ComponentProps<typeof Feather>['name']
+  color?: string
