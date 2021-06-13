64fb6d0a8ce7593c93420d309989f3da6702993f
Natanael Lima <taelima1997@gmail.com>
2021-06-13 10:21:31 -0300

    update logic routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 
-import { StackRoutes } from './stack.routes';
+import { useAuth } from '../contexts/AuthProvider';
+import { AuthRoutes } from './auth.routes';
+import { AppTabRoutes } from './app.tab.routes';
 
 export function Routes() {
+  const { user } = useAuth();
   return (
     <NavigationContainer>
-      <StackRoutes />
+      {!user.id ? <AuthRoutes /> : <AppTabRoutes />}
     </NavigationContainer>
   );
 }
