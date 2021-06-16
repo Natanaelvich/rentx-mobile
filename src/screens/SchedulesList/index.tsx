cde8630675738ec1328791a6e8b18ae86373c0fe
Natanael Lima <taelima1997@gmail.com>
2021-06-16 00:06:52 -0300

    update SchedulesList

--- src/screens/SchedulesList/index.tsx
+++ src/screens/SchedulesList/index.tsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'react-native';
 
 import { useTheme } from 'styled-components/native';
-import { useNavigation } from '@react-navigation/native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
 
+import { format, parseISO } from 'date-fns';
 import * as S from './styles';
 import { LoadAnimate } from '../../components/LoadAnimate';
 import { IconButton } from '../../components/IconButton';
@@ -14,12 +15,22 @@ import api from '../../services/api';
 export const SchedulesList = () => {
   const { colors } = useTheme();
   const { navigate } = useNavigation();
+  const isScreenFocused = useIsFocused();
+
   const [cars, setCars] = useState<CarByUser[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const loadAppointments = async () => {
     try {
-      const { data } = await api.get('/schedules_byuser?user_id=1');
+      const { data } = await api.get<CarByUser[]>(
+        '/schedules_byuser?user_id=1',
+      );
+      const formattedData = data.map(rent => ({
+        ...rent,
+        start_date: format(parseISO(rent.start_date), 'dd/MM/yyyy'),
+        end_date: format(parseISO(rent.end_date), 'dd/MM/yyyy'),
+      }));
+      setCars(formattedData);
       setCars(data);
     } catch (error) {
       console.log(error);
@@ -30,7 +41,7 @@ export const SchedulesList = () => {
 
   useEffect(() => {
     loadAppointments();
-  }, []);
+  }, [isScreenFocused]);
 
   return (
     <S.Container>
@@ -58,15 +69,15 @@ export const SchedulesList = () => {
           <S.CarsList
             data={cars}
             keyExtractor={item => String(item.id)}
-            renderItem={({ item: { car, startDate, endDate } }) => (
+            renderItem={({ item: { car, start_date, end_date } }) => (
               <S.CarWrapper>
                 <Car data={car} />
                 <S.CarFooter>
                   <S.CarFooterTitle>Período</S.CarFooterTitle>
                   <S.CarFooterPeriod>
-                    <S.CarFooterDate>{startDate}</S.CarFooterDate>
+                    <S.CarFooterDate>{start_date}</S.CarFooterDate>
                     <S.ArrowIcon />
-                    <S.CarFooterDate>{endDate}</S.CarFooterDate>
+                    <S.CarFooterDate>{end_date}</S.CarFooterDate>
                   </S.CarFooterPeriod>
                 </S.CarFooter>
               </S.CarWrapper>
