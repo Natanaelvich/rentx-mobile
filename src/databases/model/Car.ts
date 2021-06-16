fdad8966dc3765975be5d3b58092bf6a3bbadfe2
Natanael Lima <taelima1997@gmail.com>
2021-06-15 23:57:10 -0300

    add car Model

---
+++ src/databases/model/Car.ts
@@ -0,0 +1,27 @@
+import { Model } from '@nozbe/watermelondb';
+import { field } from '@nozbe/watermelondb/decorators';
+
+export class Car extends Model {
+  static table = 'cars';
+
+  @field('name')
+  name!: string;
+
+  @field('brand')
+  brand!: string;
+
+  @field('about')
+  about!: string;
+
+  @field('fuel_type')
+  fuel_type!: string;
+
+  @field('period')
+  period!: string;
+
+  @field('price')
+  price!: number;
+
+  @field('thumbnail')
+  thumbnail!: string;
+}
