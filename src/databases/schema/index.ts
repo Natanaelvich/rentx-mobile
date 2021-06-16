67c376bf3a92071f0e90620d08b43c9172044a9e
Natanael Lima <taelima1997@gmail.com>
2021-06-15 23:57:31 -0300

    update with carSchema

--- src/databases/schema/index.ts
+++ src/databases/schema/index.ts
@@ -1,8 +1,9 @@
 import { appSchema } from '@nozbe/watermelondb';
 
 import { userSchema } from './userSchema';
+import { carSchema } from './carSchema';
 
 export const schemas = appSchema({
-  version: 1,
-  tables: [userSchema],
+  version: 6,
+  tables: [userSchema, carSchema],
 });
