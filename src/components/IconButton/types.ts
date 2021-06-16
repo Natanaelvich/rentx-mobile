796cb9ff1547a364e45fb5dc404c58b1ec85afea
Natanael Lima <taelima1997@gmail.com>
2021-06-16 00:07:37 -0300

    fix eslint errror

--- src/components/IconButton/types.ts
+++ src/components/IconButton/types.ts
@@ -1,8 +1,9 @@
-import { ComponentProps } from 'react'
+import { ComponentProps } from 'react';
 
-import { Feather } from '@expo/vector-icons'
-import { BorderlessButtonProps } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons';
+import { BorderlessButtonProps } from 'react-native-gesture-handler';
 
 export type IconButtonProps = BorderlessButtonProps & {
-  name?: ComponentProps<typeof Feather>['name']
-  color?: string
+  name?: ComponentProps<typeof Feather>['name'];
+  color?: string;
+};
